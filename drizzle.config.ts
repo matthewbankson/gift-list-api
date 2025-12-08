39a1a88f874d12cd1aa28587595cc7d293964489
matthewbankson <86269497+matthewbankson@users.noreply.github.com>
2025-12-08 18:19:31 -0500

    update drizzle to use push

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 export default defineConfig({
   out: './drizzle',
-  schema: './src/db/schema.ts',
+  schema: './src/db/schema',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_CONNECTION_STRING!,
